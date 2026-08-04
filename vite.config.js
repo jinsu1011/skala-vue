e55635a7923743756f44cd339af4780da100978c
jinsoo kim <jinsu101151@gmail.com>
2026-08-04 15:19:31 +0900

    feat: GitHub Pages 배포 설정 및 스크립트 추가

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/skala-vue/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
